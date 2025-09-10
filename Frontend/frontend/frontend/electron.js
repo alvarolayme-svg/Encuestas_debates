2329ba0820e9bc4fde1bef7058344f329db62252
Alvaro <alvaro.layme@sistemas.edu.bo>
2025-09-10 17:49:08 -0400

    Commit backend frontend

---
+++ Frontend/frontend/frontend/electron.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5173"); // puerto default de Vite
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
